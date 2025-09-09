dc911f0e37a0600a54b3104b9e4f3d3b6fbe0eb4
Amr Kahla <amr.kahla@contellect.com>
2025-09-09 12:22:06 +0300

    update classifier model name

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import logging
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from backend.core.logging_config import configure_logging
 from backend.api import routes_ocr, routes_health
 
-# Configure logging at startup
+
 configure_logging()
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Document AI API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
@@ -30,5 +39,5 @@ async def log_requests(request: Request, call_next):
         raise
 
 # Routers
-app.include_router(routes_ocr.router, prefix="/api")
-app.include_router(routes_health.router, prefix="/api")
+app.include_router(routes_ocr.router, prefix="/api", tags=["Document Analyzing"])
+app.include_router(routes_health.router, prefix="/api", tags=["Health Check"])
